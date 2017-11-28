649800ad3ded267c330d8a17face4bfbddbbe50b
Chini Sinha <chinisinha@gmail.com>
2017-11-28 01:30:50 -0500

    Chini: More to the front page

---
+++ Assets/Scripts/InitializeProject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeProject : MonoBehaviour {
+
+    public GameObject openPanel;
+    public GameObject helpPanel;
+
+	void Start () {
+        openPanel.SetActive(false);
+        helpPanel.SetActive(false);
+
+        // Useful for Settings - gives the path of the Document folder.
+        //string DocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+        //Debug.Log(DocumentsPath);
+
+    }
+
+}
---
+++ Assets/Scripts/ProjectPanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ProjectPanel : MonoBehaviour
+{
+ 
+    public GameObject openPanel;
+    public GameObject mainPanel;
+    public GameObject helpPanel;
+    public GameObject settingsPanel;
+
+    public InputField projectName;
+    public InputField defaultPath;
+
+    public void OpenProjectPane()
+    {
+        openPanel.SetActive(true);
+        mainPanel.SetActive(false);
+        helpPanel.SetActive(false);
+    }
+
+    public void helpOnClick()
+    {
+        helpPanel.SetActive(true);
+        mainPanel.SetActive(false);
+        openPanel.SetActive(false);
+    }
+
+    public void LoadEditor(int index)
+    {
+        Debug.Log(projectName.text);
+        if (projectName.text == "")
+        {
+            //projectName.placeholder = "Please Enter Project Name!!";
+            projectName.placeholder.color = Color.red;
+        }
+        else
+        {
+            //TODO: Create the files and then load the scene
+            SceneManager.LoadScene(index);
+        }
+    }
+
+    public void cancelAction()
+    {
+        openPanel.SetActive(false);
+        mainPanel.SetActive(true);
+        helpPanel.SetActive(false);
+        projectName.placeholder.color = Color.gray;
+    }
+}
