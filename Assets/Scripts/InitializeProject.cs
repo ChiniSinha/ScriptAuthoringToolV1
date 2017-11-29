18c1280ae8a2d183e951896b0883790ba24c79e7
Chini Sinha <chinisinha@gmail.com>
2017-11-29 02:40:43 -0500

    Chini: Some more progress

--- Assets/Scripts/InitializeProject.cs
+++ Assets/Scripts/InitializeProject.cs
@@ -1,20 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using UnityEngine;
 
 public class InitializeProject : MonoBehaviour {
 
     public GameObject openPanel;
     public GameObject helpPanel;
+    public GameObject settingsPanel;
 
 	void Start () {
         openPanel.SetActive(false);
         helpPanel.SetActive(false);
+        settingsPanel.SetActive(false);
 
-        // Useful for Settings - gives the path of the Document folder.
-        //string DocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
-        //Debug.Log(DocumentsPath);
+        CreateAndSaveDocumentLocation();
 
     }
 
+    void CreateAndSaveDocumentLocation()
+    {
+        if (!File.Exists(Application.persistentDataPath + "/defaultProjectValues.dat"))
+        {
+            string documentsPath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            Debug.Log(documentsPath);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/defaultProjectValues.dat");
+
+            ProjectData projectData = new ProjectData();
+            projectData.filePath = documentsPath;
+
+            bf.Serialize(file, projectData);
+            file.Close();
+        }
+    }
+}
+
+[Serializable]
+class ProjectData
+{
+    public string filePath;
 }
--- Assets/Scripts/ProjectPanel.cs
+++ Assets/Scripts/ProjectPanel.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -20,6 +21,7 @@ public class ProjectPanel : MonoBehaviour
         openPanel.SetActive(true);
         mainPanel.SetActive(false);
         helpPanel.SetActive(false);
+        settingsPanel.SetActive(false);
     }
 
     public void helpOnClick()
@@ -27,6 +29,15 @@ public class ProjectPanel : MonoBehaviour
         helpPanel.SetActive(true);
         mainPanel.SetActive(false);
         openPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void settingsOnClick()
+    {
+        helpPanel.SetActive(false);
+        mainPanel.SetActive(false);
+        openPanel.SetActive(false);
+        settingsPanel.SetActive(true);
     }
 
     public void LoadEditor(int index)
@@ -34,7 +45,6 @@ public class ProjectPanel : MonoBehaviour
         Debug.Log(projectName.text);
         if (projectName.text == "")
         {
-            //projectName.placeholder = "Please Enter Project Name!!";
             projectName.placeholder.color = Color.red;
         }
         else
@@ -49,6 +59,54 @@ public class ProjectPanel : MonoBehaviour
         openPanel.SetActive(false);
         mainPanel.SetActive(true);
         helpPanel.SetActive(false);
+        settingsPanel.SetActive(false);
         projectName.placeholder.color = Color.gray;
+        settingsPanel.GetComponentInChildren<Text>().text = "";
+    }
+
+    public void getDefaultPath()
+    {
+        if (File.Exists(Application.persistentDataPath + "/defaultProjectValues.dat"))
+        {
+            BinaryFormatter bf1 = new BinaryFormatter();
+            FileStream file1 = File.OpenRead(Application.persistentDataPath + "/defaultProjectValues.dat");
+            ProjectData pd = (ProjectData)bf1.Deserialize(file1);
+            file1.Close();
+
+            Debug.Log("saved path = " + pd.filePath);
+            defaultPath.text = pd.filePath;
+        }
+    }
+
+    public void updateDefaultPath()
+    {
+        if (File.Exists(Application.persistentDataPath + "/defaultProjectValues.dat"))
+        {
+            string documentsPath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            Debug.Log(documentsPath);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            ProjectData projectData = new ProjectData();
+            projectData.filePath = settingsPanel.GetComponentInChildren<InputField>().text;
+            Debug.Log("Updated Path: " + projectData.filePath);
+
+
+            FileStream file = File.Create(Application.persistentDataPath + "/defaultProjectValues.dat");
+            bf.Serialize(file, projectData);
+            file.Close();
+
+            settingsPanel.GetComponentInChildren<Text>().text = "Update Successful!";
+        }
+
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    void CreateProjectMetaDataAndFiles()
+    {
+
     }
 }
