dd7a603f11f5eba15013c349984bca41173e32b6
Lazlo Ring <lazloring@gmail.com>
2017-12-14 16:04:51 -0500

    Fix?

---
+++ Assets/AgentScripts/UIElements/Components/FixedSizeText.cs
@@ -0,0 +1,35 @@
+﻿namespace UnityEngine.UI
+{
+    public class FixedSizeText : Text
+    {
+        public override float minHeight
+        {
+            get
+            {
+                if (horizontalOverflow == HorizontalWrapMode.Wrap)
+                {
+                    return preferredHeight;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public override float minWidth
+        {
+            get
+            {
+                if (verticalOverflow == VerticalWrapMode.Truncate)
+                {
+                    return preferredWidth;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
